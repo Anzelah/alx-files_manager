e4bccd078518dce4f225ccd4ffc3f22a31e3d0e6
Anzelah <jepkoechanzellah@gmail.com>
2024-07-29 13:05:05 -0700

    finally connected to redis server

---
+++ utils/redis.js
@@ -0,0 +1,38 @@
+import { createClient } from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.isClientConnected = true;
+    this.getAsync = promisify(this.client.get).bind(this.client);
+
+    this.client.on('error', (error) => {
+      console.log(`Redis Client not connected to server: ${error.message}`);
+      this.isClientConnected = false;
+    });
+
+    this.client.on('connect', () => {
+      this.isClientConnected = true;
+    });
+  }
+
+  isAlive() {
+    return this.isClientConnected;
+  }
+
+  async get(key) {
+    return this.getAsync(key);
+  }
+
+  async set(key, value, duration) {
+    this.client.setex(key, duration, value);
+  }
+
+  async del(key) {
+    this.client.del(key);
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
