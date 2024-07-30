53ba622d525b0f6cb56ba9f15e3ec61cf3667d16
Anzelah <jepkoechanzellah@gmail.com>
2024-07-30 08:58:14 -0700

    I was correct all along. Also, reused previous code

--- utils/db.js
+++ utils/db.js
@@ -1,39 +1,34 @@
 import { MongoClient } from 'mongodb';
-import 'process';
 
-
-const host = process.env.DB_HOST || 'localhost'
-const port = process.env.DB_PORT || 27017
-const database = process.env.DB_DATABASE || 'files_manager'
-
+const host = process.env.DB_HOST || 'localhost';
+const port = process.env.DB_PORT || 27017;
+const database = process.env.DB_DATABASE || 'files_manager';
+const url = `mongodb://${host}:${port}/`;
 
 class DBClient {
   constructor() {
-    const uri = `mongodb://${host}:${port}`
-    this.client = new MongoClient(uri, { useUnifiedTopology: true })
-
-    this.client.connect()
-      .then(() => {
-        console.log('Connected to server succesfully')
-        this.db = this.client.db(database)
-	this.db.createCollection('users')
-	this.db.createCollection('files')
-      })
-      .catch((err) => {
-        console.error(`Could not connect to server: ${err.message}`)
+    MongoClient.connect(url, { useUnifiedTopology: true }, (error, client) => {
+      if (!error) {
+        this.db = client.db(database);
+        this.usersCol = this.db.collection('users');
+        this.filesCol = this.db.collection('files');
+      } else {
+        console.log(error.message);
+        this.db = false;
+      }
     });
-    
   }
+
   isAlive() {
-    return this.client.topology.isConnected()
+    return Boolean(this.db);
   }
 
   async nbUsers() {
-    return this.db.collection('users').countDocuments()
+    return this.usersCol.countDocuments();
   }
 
   async nbFiles() {
-    return this.db.collection('files').countDocuments()
+    return this.filesCol.countDocuments();
   }
 }
 
