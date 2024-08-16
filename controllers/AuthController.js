becd5e346750202f374fb2f9924837f7dd093d45
Anzelah <jepkoechanzellah@gmail.com>
2024-08-16 09:03:10 -0700

    refractored code to use class instead of variables

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -9,31 +9,35 @@ function generateToken() {
   return uuid();
 }
 
-export const getConnect = async (req, res) => {
-  const authString = req.headers.authorization;
-  const encoded = authString.split(' ')[1];
-  const bytes = base64.decode(encoded);
-  const decodedStr = utf8.decode(bytes);
-  const email = decodedStr.split(':')[0];
+class AuthController {
+  static async getConnect(req, res) {
+    const authString = req.headers.authorization;
+    const encoded = authString.split(' ')[1];
+    const bytes = base64.decode(encoded);
+    const decodedStr = utf8.decode(bytes);
+    const email = decodedStr.split(':')[0];
 
-  const user = await dbClient.findUserbyEmail(email);
-  if (!user) {
-    return res.status(401).json({ error: 'Unauthorized' });
+    const user = await dbClient.findUserbyEmail(email);
+    if (!user) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+    const token = generateToken();
+    const key = `auth_${token}`;
+    await redisClient.set(key, 86400, user._id);
+    return res.status(200).json({ token });
   }
-  const token = generateToken();
-  const key = `auth_${token}`;
-  await redisClient.set(key, 86400, user._id);
-  return res.status(200).json({ token });
-};
 
-export const getDisconnect = async (req, res) => {
-  const token = req.headers['x-token'];
-  const key = `auth_${token}`;
-  const userId = await redisClient.get(key);
-  const user = await dbClient.findUserbyId(userId);
-  if (!user) {
-    return res.status(401).json({ error: 'Unauthorized' });
+  static async getDisconnect(req, res) {
+    const token = req.headers['x-token'];
+    const key = `auth_${token}`;
+    const userId = await redisClient.get(key);
+    const user = await dbClient.findUserbyId(userId);
+    if (!user) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+    await redisClient.del(key);
+    return res.status(204).send();
   }
-  await redisClient.del(key);
-  return res.status(204).send();
-};
+}
+
+export default new AuthController();
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,18 @@
+import AuthController from '../controllers/AuthController';
+
 const express = require('express');
 
 const router = express.Router();
 
 const { getStatus, getStats } = require('../controllers/AppController');
 const { postNew, getMe } = require('../controllers/UsersController');
-const { getConnect, getDisconnect } = require('../controllers/AuthController');
 
 router.get('/status', getStatus);
 router.get('/stats', getStats);
 router.post('/users', postNew);
 
-router.get('/connect', getConnect);
-router.get('/disconnect', getDisconnect);
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', getMe);
 
 module.exports = router;
