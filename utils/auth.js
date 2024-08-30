27edc9195a369c007da901e0c9120d292df0c32b
Anzelah <jepkoechanzellah@gmail.com>
2024-08-30 12:56:36 -0700

    eslint update

--- utils/auth.js
+++ utils/auth.js
@@ -1,5 +1,5 @@
-import redisClient from './redis'
-import dbClient from './db'
+import redisClient from './redis';
+import dbClient from './db';
 
 export default async function authUser(req, res) {
   try {
@@ -7,8 +7,8 @@ export default async function authUser(req, res) {
     const key = `auth_${token}`;
     const userId = await redisClient.get(key);
     const user = await dbClient.findUserbyId(userId);
-    return user
-  } catch(err) {
+    return user;
+  } catch (err) {
     console.error('Error retrieving user by token:', err.message);
   }
 }
